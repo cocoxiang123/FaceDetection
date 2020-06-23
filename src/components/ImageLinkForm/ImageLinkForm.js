8d21b0ca83e1e848092c9d09647293c65ac45587
Your Name <you@example.com>
2020-06-23 17:00:32 +1000

    add hints

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -6,9 +6,11 @@ function ImageLinkForm({ input, onChangeInput, onSubmit, err }) {
         <div>
             <p className="f3 text-center">This will magically detect faces in your pictures, give it a try.</p>
             <div className="form pa4 br3 shadow-5 row m-auto">
-                <input className='f4 pa2 w-70 center col-12 col-md-9'
+                <span className="f4 pa2">URL:</span>
+                <input className='f4 pa2 w-70 center col-12 col-md-8'
                     value={input}
-                    onChange={onChangeInput} />
+                    onChange={onChangeInput}
+                    placeholder="e.g https://example.com/example1.jpg" />
                 <button
                     onClick={onSubmit}
                     className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple col-12 col-md-3'>
