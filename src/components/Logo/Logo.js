c99be7fb147d8dd3e09e2e6178c55c3159efe190
Your Name <you@example.com>
2020-06-23 16:30:20 +1000

    fix issue

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import Tilt from 'react-tilt'
 import './Logo.css';
 import logo from './face.png'
 function Logo() {
     return (
         <div>
-            <Tilt className="Tilt br2 shadow-2 ml-4" options={{ max: 25 }} style={{ height: 150, width: 150 }} >
+            <div className="Tilt br2 shadow-2 ml-4" options={{ max: 25 }} style={{ height: 150, width: 150 }} >
                 <div className="Tilt-inner"><img src={logo} alt="face" /></div>
-            </Tilt>
+            </div>
         </div>
     )
 }
